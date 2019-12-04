9afbe09d2cdba68282294f73c29ed9dddcb4dfb3
KerryAlsace <kerrydougherty@gmail.com>
2019-12-04 13:44:44 -0500

    finish day 1

--- 1/calculate_total_module_mass_test.go
+++ 1/calculate_total_module_mass_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCalculateModuleMass(t *testing.T) {
+func TestCalculateTotalModuleMass(t *testing.T) {
 	tests := []struct {
 		testName string
 		mass     int
@@ -12,30 +12,25 @@ func TestCalculateModuleMass(t *testing.T) {
 	}{
 		{
 			testName: "1",
-			mass:     12,
-			want:     2,
-		},
-		{
-			testName: "2",
 			mass:     14,
 			want:     2,
 		},
 		{
-			testName: "3",
+			testName: "2",
 			mass:     1969,
-			want:     654,
+			want:     966,
 		},
 		{
-			testName: "4",
+			testName: "3",
 			mass:     100756,
-			want:     33583,
+			want:     50346,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			if got := calculateModuleMass(tt.mass); got != tt.want {
-				t.Errorf("calculateModuleMass() = [%v], want [%v]", got, tt.want)
+			if got := calculateTotalModuleMass(tt.mass); got != tt.want {
+				t.Errorf("calculateTotalModuleMass() = [%v], want [%v]", got, tt.want)
 			}
 		})
 	}
--- 1/main.go
+++ 1/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	i := getInput()
 	partOne(i)
+	partTwo(i)
 }
 
 func getInput() []string {
@@ -24,26 +25,52 @@ func getInput() []string {
 func partOne(i []string) {
 	totalFuelRequired := 0
 
-	for _, module := range i {
-		totalFuelRequired += calculateModuleMass(module)
+	for _, m := range i {
+		// convert to int
+		mass, err := strconv.Atoi(m)
+		if err != nil {
+			panic(err)
+		}
+
+		totalFuelRequired += calculateModuleMass(mass)
 	}
 
 	fmt.Printf("Part 1 Answer: %v\n", totalFuelRequired)
 }
 
-func calculateModuleMass(mass string) int {
-	// convert to int
-	m, err := strconv.Atoi(mass)
-	if err != nil {
-		panic(err)
-	}
-
+func calculateModuleMass(mass int) int {
 	// divide by 3
-	n := m / 3
+	n := mass / 3
 
 	// round down to nearest whole number
 	// Note: dividing by an int in Go makes this happen automatically, don't need to code it
 
 	// subtract 2
 	return n - 2
+}
+
+func partTwo(i []string) {
+	totalFuelRequired := 0
+
+	for _, m := range i {
+		// convert to int
+		mass, err := strconv.Atoi(m)
+		if err != nil {
+			panic(err)
+		}
+
+		totalFuelRequired += calculateTotalModuleMass(mass)
+	}
+
+	fmt.Printf("Part 2 Answer: %v\n", totalFuelRequired)
+}
+
+func calculateTotalModuleMass(mass int) int {
+	fuel := calculateModuleMass(mass)
+
+	if fuel < 1 {
+		return 0
+	}
+
+	return fuel + calculateTotalModuleMass(fuel)
 }
