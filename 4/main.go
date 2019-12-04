a957935431ef65380b78778bf4781ae0f49fedbb
KerryAlsace <kerrydougherty@gmail.com>
2019-12-04 15:17:55 -0500

    part 2 day 4 complete

--- 4/main.go
+++ 4/main.go
@@ -8,11 +8,12 @@ import (
 
 var input = "193651-649729"
 
-var testInput = "234545-234567" // 6
+var testInput = "234545-234567" // 6 for part 1, 5 for part 2
 
 func main() {
 	i := input
 	partOne(i)
+	partTwo(i)
 }
 
 func partOne(i string) {
@@ -76,9 +77,64 @@ func hasDoubleDigits(s string) bool {
 
 func hasAscendingDigits(s string) bool {
 	h := true
+
 	for i := 0; i < 5; i++ {
 		if s[i] > s[i+1] {
 			h = false
+		}
+	}
+
+	return h
+}
+
+func partTwo(i string) {
+	fmt.Printf("Part 2 Answer: %v\n", findMoreRestrictedNumPossible(i))
+}
+
+func findMoreRestrictedNumPossible(i string) int {
+	var possiblePasswords int
+	rs := strings.Split(i, "-")
+	l, u := getUpperAndLowerBounds(rs)
+
+	r := u - l
+
+	for i := 0; i < r; i++ {
+		n := l + i
+
+		ns := strconv.Itoa(n)
+
+		if !hasAscendingDigits(ns) {
+			continue
+		}
+
+		if !hasIsolatedDoubleDigits(ns) {
+			continue
+		}
+
+		possiblePasswords++
+	}
+
+	return possiblePasswords
+}
+
+func hasIsolatedDoubleDigits(s string) bool {
+	h := false
+
+	m := make(map[byte]int)
+
+	for i := 0; i < 5; i++ {
+		if s[i] == s[i+1] {
+			h = true
+			m[s[i]] += 2
+		}
+	}
+
+	if h {
+		h = false
+		for _, v := range m {
+			if v == 2 {
+				h = true
+			}
 		}
 	}
 
